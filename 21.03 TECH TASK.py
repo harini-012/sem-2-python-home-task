91b00538169b471efa8402eafc45a987e6205470
harini-012 <harinirajesh0519@gmail.com>
2025-03-23 11:27:13 +0530

    Add files via upload

---
+++ 21.03 TECH TASK.py
@@ -0,0 +1,8 @@
+
+'''2. Swap Two Strings Without Using a Third Variable'''
+
+a=input("Enter the first string")
+b=input("Enter the second string")
+print("Before swapping",a,b)
+a,b=b,a
+print("After swapping",a,b)
